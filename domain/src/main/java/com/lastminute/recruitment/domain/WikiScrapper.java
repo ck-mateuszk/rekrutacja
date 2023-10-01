833a5a389e897e50199281a39406e60222686795
Sehirone <kajetan123@gmail.com>
2023-10-01 16:50:59 +0200

    working implementation for reading html/json page and crawling through ref links

--- domain/src/main/java/com/lastminute/recruitment/domain/WikiScrapper.java
+++ domain/src/main/java/com/lastminute/recruitment/domain/WikiScrapper.java
@@ -1,5 +1,8 @@
 package com.lastminute.recruitment.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class WikiScrapper {
 
     private final WikiReader wikiReader;
@@ -12,7 +15,31 @@ public class WikiScrapper {
 
 
     public void read(String link) {
-
+        WikiPage wikiPage = wikiReader.read(link);
+        Set<WikiPage> wikiPagesRead = readAll(wikiPage);
+        wikiPagesRead.forEach(repository::save);
     }
 
+    private Set<WikiPage> readAll(WikiPage initialWikiPage) {
+        return getAllLinkedPages(initialWikiPage, new HashSet<>());
+    }
+
+    private Set<WikiPage> getAllLinkedPages(WikiPage mainWikiPage, Set<String> alreadyReadLinks) {
+        Set<WikiPage> result = new HashSet<>();
+
+        if (alreadyReadLinks.contains(mainWikiPage.getSelfLink())) {
+            return result;
+        }
+
+        alreadyReadLinks.add(mainWikiPage.getSelfLink());
+        result.add(mainWikiPage);
+
+        for (String nextLink : mainWikiPage.getLinks()) {
+            WikiPage nextWikiPage = wikiReader.read(nextLink);
+            result.add(nextWikiPage);
+            result.addAll(getAllLinkedPages(nextWikiPage, alreadyReadLinks));
+        }
+
+        return result;
+    }
 }
--- jsonwikiclient/src/main/java/com/lastminute/recruitment/client/JsonWikiClient.java
+++ jsonwikiclient/src/main/java/com/lastminute/recruitment/client/JsonWikiClient.java
@@ -1,12 +1,18 @@
 package com.lastminute.recruitment.client;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 public class JsonWikiClient {
 
-    public String readJson(String link) {
+    public String readJson(String link) throws IOException {
         String name = link.replace("\"", "")
                 .replace("http://wikiscrapper.test/", "/wikiscrapper/") + ".json";
-        return Objects.requireNonNull(getClass().getResource(name)).getFile();
+        InputStream inputStream = Objects.requireNonNull(getClass().getResourceAsStream(name));
+        return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
     }
 }
